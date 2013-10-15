88200cf9da1d4d9f13d314c88d890f438f6ced84
Mark Lopez <admin@silvenga.com>
2013-10-15 10:51:09 -0600

    Comments, half edited

--- src/nim/Player.java
+++ src/nim/Player.java
@@ -3,63 +3,51 @@ package nim;
 import java.util.Scanner;
 
 public class Player {
+	
 	private String name;
 	private static Scanner input;
 	
-	public Player(String name)
-	{
+	public Player(String name) {
+	
 		this.name = name;
 	}
 	
-	public Board takeTurn(Board board)
-	{
+	public Board takeTurn(Board board) {
+	
 		int row, x;
-		board.displayBoard();
+		board.displayBoard();// Should separated into two methods
 		System.out.println("Player " + name + "'s turn! Please choose a row(1, 2, or 3):");
-		while(true)
-		{
-			try
-			{
+		while (true) {
+			try {
 				row = input.nextInt();
-				if(row > 3 || row < 1)
+				if (row > 3 || row < 1)
 					throw new Exception();
 				
-				if(board.getRow(row) == 0)
-				{
+				if (board.getRow(row) == 0) {
 					System.out.println("That row is already empty! Please try another.");
 					throw new Exception();
 				}
-					
-					
+				
 				break;
-			}
-			catch(Exception e)
-			{
+			} catch (Exception e) {
 				System.err.println("Invalid Input");
 				System.out.println("You goofed. Please choose an integer 1, 2, or 3:");
 			}
 		}
 		
 		System.out.println("You have chosen row " + row + ". Now please choose a number to remove between 1 and " + board.getRow(row));
-		while(true)
-		{
-			try
-			{
+		while (true) {
+			try {
 				x = input.nextInt();
-				if(x > 0 && x <= board.getRow(row))
-				{
+				if (x > 0 && x <= board.getRow(row)) {
 					board.makeMove(row, x);
 					System.out.println(x + " removed from row " + row + ".");
-				}
-				else
-				{
+				} else {
 					throw new Exception();
 				}
 				
 				break;
-			}
-			catch(Exception e)
-			{
+			} catch (Exception e) {
 				System.err.println("Invalid Input");
 				System.out.println("You goofed. Please choose an integer between 1 and " + board.getRow(row));
 			}
